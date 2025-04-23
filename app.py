16e19d5370f401b3bb43dfff69120b7a34dca359
Venkatsai2M <128076476+Venkatsai2M@users.noreply.github.com>
2025-04-23 22:15:11 +0530

    intial commit

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def query():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5001)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port)
